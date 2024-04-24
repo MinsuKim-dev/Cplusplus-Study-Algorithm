a7e0feb3b043b7c60c63bce0b1d1f0763801a7f1
Minsu Kim-dev <710mensu@gmail.com>
2024-04-24 18:45:49 +0900

    공부 내용 추가

--- Maze/Maze.cpp
+++ Maze/Maze.cpp
@@ -1,37 +1,34 @@
 ﻿#include <iostream>
 #include "pch.h"
 #include "ConsoleHelper.h"
+#include "Board.h"
+#include "Player.h"
+
+
+Board board;
+Player player;
 
 int main()
 {
+    ::srand(static_cast<unsigned>(time(nullptr)));
+    board.Init(25, &player);
+    player.Init(&board);
+
     uint64 lastTick = 0;
     while (true)
     {
 #pragma region 프레임 관리
         const uint64 currentTick = ::GetTickCount64();
-        const uint64 deltaTick = currentTick - lastTick;
+        const uint64 deltaTick = currentTick - lastTick; // 프레임마다 경과 시간
         lastTick = currentTick;
 #pragma endregion
 
         // 입력
 
         // 로직
+        player.Update(deltaTick);
 
         // 렌더링
-
-        ConsoleHelper::SetCursorPosition(0, 0);
-        ConsoleHelper::ShowConsoleCursor(false);
-        ConsoleHelper::SetCursorColor(ConsoleColor::RED);
-        
-        const char* TILE = "■";
-
-        for (int32 y = 0; y < 25; y++)
-        {
-            for (int32 x = 0; x < 25; x++)
-            {
-                cout << TILE;
-            }
-            cout << endl;
-        }
+        board.Render();
     }
 }
